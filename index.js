757a5a1a96a7dfb9c42873810b60b52dccb28c68
primaoriondigital <126743155+primaoriondigital@users.noreply.github.com>
2023-03-28 15:40:29 +0700

    update server

--- index.js
+++ index.js
@@ -12,12 +12,22 @@ app.use(morgan("dev"))
 app.use(bodyParser.json())
 
 app.use("/", mainRouter)
-app.all("*",(req,res,next) => {
-  response(res,404,false,null,"404 Not Found")
+// app.all("*",(req,res,next) => {
+//   response(res,404,false,null,"404 Not Found")
+// })
+// app.get("/", (req, res, next) => {
+//   res.status(200).json({ status: "success", statusCode: 200 });
+// });
+
+const server = http.createServer((req,res) => {
+    if(req.url == '/hello') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end('Hello world')
+    }else{
+        res.writeHead(200,{'Content-Type': 'application/json'})
+        res.end('OK')
+    }
 })
-app.get("/", (req, res, next) => {
-  res.status(200).json({ status: "success", statusCode: 200 });
-});
 
 const port = process.env.PORT
 
